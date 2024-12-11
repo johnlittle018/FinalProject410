e9ccd1f9404395f98545f2d440ae1f21b9faea92
johnlittle018 <jl668124@hotmail.com>
2024-12-11 17:25:23 -0500

    Initial commit

---
+++ src/test/java/TestMp4Player.java
@@ -0,0 +1,16 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class TestMp4Player {
+
+        @Test
+        public void testSample() {
+
+                Assert.assertEquals(3, 3);
+
+
+        }
+}
