ce4838522492ab4569c04b7ab4895d63dcb35d2c
johnlittle018 <jl668124@hotmail.com>
2024-12-12 21:44:51 -0500

    Update PassengersTest.java

--- src/test/java/TruckAndCar/PassengersTest.java
+++ src/test/java/TruckAndCar/PassengersTest.java
@@ -15,14 +15,14 @@ public class PassengersTest {
 
         Passenger p1 = new Passenger(50);
         Passenger p2 = new Passenger(50);
-        Passenger p3 = new Passenger(80);
+
 
         passengers.addPassenger(p1);
         passengers.addPassenger(p2);
 
         List<Passenger> testList = new ArrayList<>();
         testList.add(p1);
-        testList.add(p3);
+        testList.add(p2);
 
         Assertions.assertEquals(testList, passengers.getPassengers());
 
